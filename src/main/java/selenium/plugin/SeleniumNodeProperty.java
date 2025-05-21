45357c7deec8fd6108e88b2a53320a33f9d8b41d
julian.boehne <julian.boehne@itx.de>
2025-05-21 13:37:01 +0200

    chore: preparation for setup agents

--- src/main/java/selenium/plugin/SeleniumNodeProperty.java
+++ src/main/java/selenium/plugin/SeleniumNodeProperty.java
@@ -4,10 +4,11 @@ import hudson.Extension;
 import hudson.model.Node;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.FormValidation;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 public class SeleniumNodeProperty extends NodeProperty<Node> {
-
     private final boolean enabled;
 
     @DataBoundConstructor
@@ -21,7 +22,6 @@ public class SeleniumNodeProperty extends NodeProperty<Node> {
 
     @Extension
     public static final class DescriptorImpl extends NodePropertyDescriptor {
-
         @Override
         public String getDisplayName() {
             return "Selenium-Node";
@@ -30,6 +30,11 @@ public class SeleniumNodeProperty extends NodeProperty<Node> {
         @Override
         public boolean isApplicable(Class<? extends Node> nodeType) {
             return true;
+        }
+
+        public FormValidation doStartNode(@QueryParameter boolean enabled) {
+            // Your logic to start the Selenium server
+            return FormValidation.ok("Selenium Node wurde gestartet.");
         }
     }
 }
