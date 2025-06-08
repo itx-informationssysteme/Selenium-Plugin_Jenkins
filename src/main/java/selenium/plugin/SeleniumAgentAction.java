66dc4fe4a0ea78db2e7d9c12b952f9329bb652c4
julian.boehne <julian.boehne@itx.de>
2025-06-08 16:00:02 +0200

    feat(agent): setup settings structure

--- src/main/java/selenium/plugin/SeleniumAgentAction.java
+++ src/main/java/selenium/plugin/SeleniumAgentAction.java
@@ -2,10 +2,15 @@ package selenium.plugin;
 
 import hudson.model.Action;
 import hudson.model.Computer;
+import hudson.model.ManagementLink;
+import org.kohsuke.stapler.HttpRedirect;
+import org.kohsuke.stapler.HttpResponse;
 
 public class SeleniumAgentAction implements Action {
 
     private final Computer computer;
+
+    private boolean nodeActive;
 
     public SeleniumAgentAction(Computer computer) {
         this.computer = computer;
@@ -30,11 +35,23 @@ public class SeleniumAgentAction implements Action {
         return computer;
     }
 
-    public boolean isEnabled() {
-        return true;
+    public String getVersion() {
+        if (ManagementLink.all().get(SeleniumGlobalProperty.class).getSeleniumVersion() == null) return "No Version set (Change in Selenium Global Configuration)";
+        return ManagementLink.all().get(SeleniumGlobalProperty.class).getSeleniumVersion();
     }
 
-    public void doEnable() {
-        // Settings
+    public HttpResponse doStartNode() {
+        nodeActive = true;
+        return new HttpRedirect(".");
     }
+
+    public HttpResponse doStopNode() {
+        nodeActive = false;
+        return new HttpRedirect(".");
+    }
+
+    public boolean getNodeActive() {
+        return nodeActive;
+    }
+
 }
